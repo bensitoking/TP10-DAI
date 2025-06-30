e0190c8e062c3b9519d240fef3ec4f3804deeb9d
Android Studio <devandroid@ort>
2025-06-30 10:28:30 -0300

    config hecha creo

--- src/configs/db-configs.js
+++ src/configs/db-configs.js
@@ -1,9 +1,20 @@
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres', 
-    password: 'root',      
-    port: 5430
+import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pkg;
+
+const DBConfigBest = {
+  host     : process.env.DB_HOST,
+  database : process.env.DB_DATABASE,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  port     : process.env.DB_PORT ?? 5432,
+  ssl: {
+    rejectUnauthorized: false, // 🔐 necesario para Supabase
+  },
 };
 
-export default config;
+const pool = new Pool(DBConfigBest);
+
+export default pool;
